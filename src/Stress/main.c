5a46390f94a253fc035c4992d877ceb77a009011
evin@raspberrypi <evinodisho@gmail.com>
2025-11-21 00:29:06 -0800

    threading supported

--- src/Stress/main.c
+++ src/Stress/main.c
@@ -8,7 +8,11 @@ int main(int argc, char **argv) {
     pthread_t thread1, thread2, thread3, thread4;
 
     pthread_create(&thread1, NULL, intrate_alu, NULL);
+    pthread_create(&thread2, NULL, intrate_alu, NULL);
+    // pthread_create(&thread3, NULL, intrate_alu, NULL);
+    // pthread_create(&thread4, NULL, intrate_alu, NULL);
 
     pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
     return 0;
 }
