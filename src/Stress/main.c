741dc793b565700c3a45e541b4d575594540feff
evin@raspberrypi <evinodisho@gmail.com>
2025-11-21 00:43:51 -0800

    Runs on 4 threads in parallel. Added sleeps between each thread to see stepping in 'CPU Util' panel in dashboard

--- src/Stress/main.c
+++ src/Stress/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "int_stress.h"
 
 int main(int argc, char **argv) {
@@ -7,12 +8,20 @@ int main(int argc, char **argv) {
 
     pthread_t thread1, thread2, thread3, thread4;
 
+    // Starting threads with sleeps in between to
+    // see incremental "step-up" in CPU Util
     pthread_create(&thread1, NULL, intrate_alu, NULL);
+    sleep(5);
     pthread_create(&thread2, NULL, intrate_alu, NULL);
-    // pthread_create(&thread3, NULL, intrate_alu, NULL);
-    // pthread_create(&thread4, NULL, intrate_alu, NULL);
+    sleep(5);
+    pthread_create(&thread3, NULL, intrate_alu, NULL);
+    sleep(5);
+    pthread_create(&thread4, NULL, intrate_alu, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+
     return 0;
 }
