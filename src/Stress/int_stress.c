483848a43dabec779f124ca824eef57a00eb45d1
Evin <evinodisho@gmail.com>
2025-11-20 23:27:27 -0800

    ALU stress

--- src/Stress/int_stress.c
+++ src/Stress/int_stress.c
@@ -1,5 +1,33 @@
+#include <time.h>
 #include "int_stress.h"
 
 double intrate_alu(uint64_t iterations) {
-    return 0;
+    // Prevent optimization removing work
+    static volatile uint64_t intrate_sink = 0;
+
+    uint64_t a = 1;
+    uint64_t b = 2;
+    uint64_t c = 3;
+    uint64_t d = 4;
+
+    clock_t start = clock();
+
+    for (uint64_t i = 0; i < iterations; i++) {
+        a += 3;
+        b ^= a;
+        c *= b;
+        d = (d << 2) + (c >> 1);
+        a ^= (d * 3);
+        c += (a >> 3);
+    }
+
+    clock_t end = clock();
+
+    intrate_sink = a + b + c + d;
+
+
+    double seconds = (end - start) / CLOCKS_PER_SEC;
+
+
+    return seconds;
 }
--- src/Stress/main.c
+++ src/Stress/main.c
@@ -4,5 +4,9 @@
 int main(int argc, char **argv) {
     printf("Hello world\n");
 
+    uint64_t iterations = 3ULL * 1000ULL * 1000ULL * 1000ULL; // 3 billion
+    double ticks = intrate_alu(iterations);
+    printf("ALU stress took %lu ticks in cpu-time.\n", ticks);
+
     return 0;
 }
